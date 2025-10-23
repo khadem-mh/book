ab0cc36682b51250d2d7e148bc9e792545315bf3
khadem-mh <khadem13359@gmail.com>
2025-10-23 23:12:36 +0330

    Test

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -9,7 +9,7 @@ const Books = () => {
     <Card className="shadow-none">
       <h1 className="text-2xl font-bold mb-6">کتاب‌ها</h1>
 
-      <CardBody className="grid grid-cols-2 py-10">
+      <CardBody className="grid sm:grid-cols-3 py-10">
         {sampleBooks.map((book) => (
           <BookCard key={book.id} book={book} />
         ))}
--- src/components/books/BookCard.tsx
+++ src/components/books/BookCard.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import { Book } from '@/types';
-import Book3D from './Book3D';
+import { Button, Divider } from '@heroui/react';
+import { FaStar } from 'react-icons/fa';
 import { LuBookOpenText, LuCalendarClock } from 'react-icons/lu';
 import { MdSignalCellularAlt } from 'react-icons/md';
 
@@ -16,16 +19,16 @@ export default function BookCard({ book }: BookCardProps) {
   };
 
   return (
-    <div className="bg-white shadow-[0_0_10px_lightgray] rounded-xl p-4 flex flex-col gap-6 items-center">
-      {/* کتاب سه‌بعدی */}
-      <Book3D book={book} />
-
-      <div className='mt-2'>
+    <div className="bg-white shadow-lg rounded-2xl p-4 flex flex-col gap-6 items-center">
+      <img src={book.cover} alt="cover" className='w-60 rounded-xl' />
+      <div className='w-full px-14'>
+        <Divider className='bg-slate-200' />
+      </div>
+      <div className=''>
         <div className="flex flex-col flex-1 text-right">
           {/* عنوان و نویسنده */}
           <h3 className="w-ful flex flex-col items-center gap-2">
-            <p className='text-xl font-bold'>{book.title}</p>
-            <p className="font-normal text-gray-500">{book.titleEn}</p>
+            <p className='text-xl font-bold'>کتاب {book.title}</p>
           </h3>
 
           {/* توضیحات کوتاه */}
@@ -33,9 +36,34 @@ export default function BookCard({ book }: BookCardProps) {
             <p className="text-sm text-gray-500 mt-3 line-clamp-3">{book.description}</p>
           )}
 
-          <div className="mt-4 flex gap-3 text-sm">
+          {/* نویسنده‌ها */}
+          <div className='flex items-center justify-between mt-4'>
+            <p className='text-slate-600 text-sm'>نویسندگان</p>
+            <div className="flex items-center gap-4">
+              {book.authors.map((author) => (
+                <div key={author.name} className="flex items-center gap-1.5">
+                  <img
+                    src={author.cover}
+                    alt={author.name}
+                    className="w-6.5 h-6.5 rounded-lg object-cover border border-gray-200"
+                  />
+                  <span className="text-gray-600 text-sm">{author.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className='flex items-center justify-between mt-4'>
+            <p className='text-slate-600 text-sm'>امتیاز</p>
+            <div className="w-fit flex items-center gap-1">
+              {[...Array(5)].map((_, i) => (
+                <FaStar className='text-yellow-500' />
+              ))}
+            </div>
+          </div>
+          <div className="mt-4 flex flex-wrap gap-3 text-sm">
             {/** سال انتشار */}
-            <div className="min-w-32 w-fit h-fit px-4 py-2 flex flex-col items-center bg-white rounded-xl shadow-sm hover:shadow-md transition-shadow">
+            <div className="flex-1 min-w-32 w-fit h-fit px-4 py-2 flex flex-col items-center bg-white rounded-xl shadow-sm hover:shadow-md transition-shadow">
               <div className="flex items-center gap-1 text-gray-500">
                 <LuCalendarClock size={18} />
                 <span>سال انتشار</span>
@@ -44,7 +72,7 @@ export default function BookCard({ book }: BookCardProps) {
             </div>
 
             {/** تعداد صفحات */}
-            <div className="min-w-32 w-fit h-fit px-4 py-2 flex flex-col items-center bg-white rounded-xl shadow-sm hover:shadow-md transition-shadow">
+            <div className="flex-1 min-w-32 w-fit h-fit px-4 py-2 flex flex-col items-center bg-white rounded-xl shadow-sm hover:shadow-md transition-shadow">
               <div className="flex items-center gap-1 text-gray-500">
                 <LuBookOpenText size={18} />
                 <span>تعداد صفحات</span>
@@ -53,7 +81,7 @@ export default function BookCard({ book }: BookCardProps) {
             </div>
 
             {/** سطح */}
-            <div className="min-w-32 w-fit h-fit px-4 py-2 flex flex-col items-center bg-white rounded-xl shadow-sm hover:shadow-md transition-shadow">
+            <div className="flex-1 min-w-32 w-fit h-fit px-4 py-2 flex flex-col items-center bg-white rounded-xl shadow-sm hover:shadow-md transition-shadow">
               <div className="flex items-center gap-1 text-gray-500">
                 <MdSignalCellularAlt size={18} />
                 <span>سطح</span>
@@ -62,40 +90,9 @@ export default function BookCard({ book }: BookCardProps) {
             </div>
           </div>
 
-
-          {/* نویسنده‌ها */}
-          <div className="flex flex-wrap gap-2 mt-2">
-            {book.authors.map((author) => (
-              <div key={author.name} className="flex items-center gap-1">
-                <img
-                  src={author.cover}
-                  alt={author.name}
-                  className="w-6 h-6 rounded-full object-cover border border-gray-200"
-                />
-                <span className="text-gray-600 text-sm">{author.name}</span>
-              </div>
-            ))}
-          </div>
-
-          {/* دسته‌بندی + رتبه */}
-          <div className="flex justify-between items-center mt-4">
-            <span className="badge badge-outline">{book.category.name}</span>
-            <div className="rating rating-sm">
-              {[...Array(5)].map((_, i) => (
-                <input
-                  key={i}
-                  type="radio"
-                  className="mask mask-star-2 bg-orange-400"
-                  checked={i < Math.round(book.rating)}
-                  readOnly
-                />
-              ))}
-            </div>
-          </div>
-
           {/* دکمه مشاهده جزئیات */}
           <div className="mt-4 flex justify-end">
-            <button className="btn btn-primary btn-sm">مشاهده جزئیات</button>
+            <Button color='primary' fullWidth variant='flat'>مشاهده جزئیات</Button>
           </div>
         </div>
       </div>
