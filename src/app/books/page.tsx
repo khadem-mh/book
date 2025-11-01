7bf8f434ff99b54d6e8d6fcb0484dd78005d4e75
khadem-mh <khadem13359@gmail.com>
2025-11-01 18:53:28 +0330

    Update

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -6,7 +6,7 @@ import BookCard from "@/components/books/BookCard";
 const Books = () => {
   return (
     <div className="px-6">
-      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 py-10">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4 xl:gap-6 py-10">
         {books.map((book, index) => (
           <BookCard key={index} book={book} />
         ))}
--- src/components/books/BookCard.tsx
+++ src/components/books/BookCard.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { FiArrowUpRight } from 'react-icons/fi';
 import { LuBrain } from 'react-icons/lu';
 import { TbBrandOpenai, TbFileTextAi } from 'react-icons/tb';
+import RatingStars from './RatingStars';
 
 interface BookCardProps {
   book: any;
@@ -14,11 +15,11 @@ export default function BookCard({ book }: BookCardProps) {
 
   return (
     <Link href={`/books/${book.slug}`}>
-      <div className="bg-white shadow-[0_0_10px_#e5e5e5] hover:shadow-none border border-slate-200 transition-shadow duration-500 hover:animate-appearance-in rounded-2xl p-4 flex flex-col gap-4 items-center">
-        <div className='w-full flex items-center justify-center'>
+      <div className="bg-white shadow-[0_0_10px_#e5e5e5] hover:shadow-none border border-slate-200 transition-shadow duration-500 hover:animate-appearance-in rounded-2xl lg:p-2 xl:p-4 flex flex-col gap-4 items-center">
+        <div className='w-full flex flex-col xl:flex-row items-center justify-center'>
           <img src={book.cover} alt="cover" className='w-60 floating-image rounded-2xl border border-slate-200' />
 
-          <div className="flex floating-box flex-col gap-4 border border-r-0 border-slate-200 rounded-l-2xl rounded-r-0 py-4 text-sm">
+          <div className="flex xl:flex-col floating-box xl:gap-4 mt-4 xl:mt-0 xl:border border-r-0 border-slate-200 rounded-l-2xl rounded-r-0 py-4 text-sm">
             {/** سال انتشار */}
             <div className="flex-1 min-w-28 w-fit h-fit flex flex-col items-center">
               <div className="bg-sky-500/6 border border-slate-200 rounded-xl p-2 flex items-center text-gray-500">
@@ -28,7 +29,7 @@ export default function BookCard({ book }: BookCardProps) {
             </div>
 
             {/** تعداد صفحات */}
-            <div className="flex-1 min-w-28 w-fit h-fit flex flex-col items-center">
+            <div className="flex-1 border-r border-l xl:border-0 border-slate-200 min-w-28 w-fit h-fit flex flex-col items-center">
               <div className="bg-black/5 border border-slate-200 rounded-xl p-2 flex items-center text-gray-500">
                 <TbBrandOpenai size={22} className='animate-spin text-black/80' style={{ animationDuration: "5500ms" }} />
               </div>
@@ -45,33 +46,33 @@ export default function BookCard({ book }: BookCardProps) {
           </div>
         </div>
 
-        <div className="flex floating-box-left flex-col flex-1 text-right bg-slate-100 rounded-2xl p-4">
+        <div className="flex flex-col flex-1 -mt-4 xl:mt-0 text-right rounded-2xl p-4">
           <p className='text-2xl w-full text-center font-[Lalezar] text-gray-700 mb-2'>{book.titleFa}</p>
           <p className="text-sm text-gray-500 mt-1 line-clamp-3">{book.description}</p>
 
-          {/* <RatingStars rating={book.rating ?? 0} /> */}
+          <RatingStars rating={book.rating ?? 0} />
 
           {/* نویسنده‌ها */}
-          {/*   <div className='flex items-center justify-between mt-4'>
-          <p className='text-slate-600 text-sm font-[Lalezar]'>نویسندگان</p>
-          <div className="flex items-center gap-4">
-            {book.authors.map((author: any) => (
-              <div key={author.name} className="flex items-center gap-1.5">
-                <img
-                  src={author.cover}
-                  alt={author.name}
-                  className="w-6.5 h-6.5 rounded-lg object-cover border border-gray-200"
-                />
-                <span className="text-gray-600 text-sm">{author.name}</span>
-              </div>
-            ))}
+          <div className='flex items-center justify-between mt-4'>
+            <p className='text-slate-600 text-sm font-[Lalezar]'>نویسندگان</p>
+            <div className="flex items-center gap-4">
+              {book.authors.map((author: any) => (
+                <div key={author.name} className="flex items-center gap-1.5">
+                  <img
+                    src={author.cover}
+                    alt={author.name}
+                    className="w-6.5 h-6.5 rounded-lg object-cover border border-gray-200"
+                  />
+                  <span className="text-gray-600 text-sm lg:hidden xl:flex">{author.name}</span>
+                </div>
+              ))}
+            </div>
           </div>
-        </div> */}
         </div>
 
-        <div className='w-full px-6'>
+        <div className='w-full xl:px-6'>
           <Button fullWidth className='border-slate-100 text-md rounded-xl' variant='bordered' startContent={<FiArrowUpRight className='text-xl animate-bounce' />}>
-            <p className='text-center animated-text'>
+            <p className='text-center animated-text truncate'>
               {book.title}
             </p>
           </Button>
