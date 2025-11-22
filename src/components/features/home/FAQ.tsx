626df6680c4bc823bd1d04796b206bf142f5ceba
khadem-mh <khadem13359@gmail.com>
2025-11-22 18:01:08 +0330

    Update

--- src/components/features/home/FAQ.tsx
+++ src/components/features/home/FAQ.tsx
@@ -18,25 +18,32 @@ const FAQ: React.FC<FAQProps> = ({ title = "سوالات متداول", items })
     <section>
       <h3 className="text-2xl font-semibold relative z-10 mb-6">{title}</h3>
 
-      <Accordion
-        variant="splitted"
-        className="rtl"
-        itemClasses={{
-          base: "rounded-2xl my-0.5 shadow-sm border border-gray-200",
-          title: "font-bold text-right cursor-pointer",
-          content: "text-right text-gray-600 leading-7",
-        }}
-      >
-        {items.map((item, index) => (
-          <AccordionItem
-            key={index + 1}
-            aria-label={`faq-${index}`}
-            title={item.q}
-          >
-            {item.a}
-          </AccordionItem>
-        ))}
-      </Accordion>
+      <div className="flex items-center gap-20">
+        <Accordion
+          variant="splitted"
+          className="rtl"
+          itemClasses={{
+            base: "rounded-2xl my-0.5 shadow-sm border border-gray-200",
+            title: "font-bold text-right cursor-pointer",
+            content: "text-right text-gray-600 leading-7",
+          }}
+        >
+          {items.map((item, index) => (
+            <AccordionItem
+              key={index + 1}
+              aria-label={`faq-${index}`}
+              title={item.q}
+            >
+              {item.a}
+            </AccordionItem>
+          ))}
+        </Accordion>
+        <img
+          src="/images/home/faq.png"
+          alt="illustration"
+          className="w-64 select-none"
+        />
+      </div>
     </section>
   );
 };
