acb3e68938b8a874ae3fb5275a22095f9d3952b2
khadem-mh <khadem13359@gmail.com>
2025-11-27 20:38:21 +0330

    Update

--- src/components/features/books/book/AudioButton.tsx
+++ src/components/features/books/book/AudioButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useRef, useState } from "react";
-import { RiVoiceAiLine } from "react-icons/ri";
+import { RiVoiceAiLine, RiPauseLine } from "react-icons/ri";
 
 type AudioButtonProps = {
   src: string;
@@ -20,7 +20,7 @@ const AudioButton: React.FC<AudioButtonProps> = ({ src }) => {
     } else {
       audioRef.current.play();
       setIsPlaying(true);
-      audioRef.current.onended = () => setIsPlaying(false); // وقتی صدا تموم شد
+      audioRef.current.onended = () => setIsPlaying(false);
     }
   };
 
@@ -30,7 +30,11 @@ const AudioButton: React.FC<AudioButtonProps> = ({ src }) => {
       className="flex items-center justify-center w-8 h-8 cursor-pointer rounded-lg bg-slate-100 hover:bg-slate-200 transition-all"
       title={isPlaying ? "Pause audio" : "Play audio"}
     >
-      <RiVoiceAiLine className="text-[20px] text-gray-600" />
+      {isPlaying ? (
+        <RiPauseLine className="text-[20px] text-gray-600" />
+      ) : (
+        <RiVoiceAiLine className="text-[20px] text-gray-600" />
+      )}
       <audio ref={audioRef} src={src} />
     </div>
   );
